1d67e74bd7c2f499f1d01512df84926b6f33d7d4
shreyansh-web <shreyansh487@gmail.com>
2024-09-05 01:26:28 +0530

    feat: add footer

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,8 @@
 import Navbar from "@/components/ui/Navbar";
 import Link from "next/link";
 import Hero from "@/components/Hero";
-import { Testimonials } from "@/components/Testimonials"; 
+import { Testimonials } from "@/components/Testimonials";
+import Footer from "@/components/Footer";
 
 export default function HomePage() {
   return (
@@ -9,6 +10,7 @@ export default function HomePage() {
       <Navbar />
       <Hero />  
       <Testimonials />
+      <Footer />
     </>
   );
 }
