baee60a02dbb32cd82672aba5faef3ca7fe8104d
shreyansh-web <shreyansh487@gmail.com>
2024-09-05 23:19:00 +0530

    feat: add dashboard

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -19,7 +19,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
-        button: "bg-[#EB5E28] text-white shadow-sm hover:bg-[#EB5E28/90] rounded-[10px]",
+        button: "bg-[#EB5E28] text-white shadow-sm hover:bg-[#e8794d] rounded-[10px]",
       },
       size: {
         default: "h-9 px-4 py-2",
