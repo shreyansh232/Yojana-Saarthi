6f4de82ae89255b65a5732a759370a3c0981aaac
shreyansh-web <shreyansh487@gmail.com>
2024-09-06 08:54:25 +0530

    fixes

--- frontend/src/components/Stepper.tsx
+++ frontend/src/components/Stepper.tsx
@@ -4,7 +4,6 @@ import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { NagrikForm } from "@/components/NagrikForm";
-import { Skeleton } from "@/components/ui/skeleton";
 import { ChevronRight, Loader2 } from "lucide-react";
 import Link from "next/link";
 import axios from "axios";
@@ -12,16 +11,16 @@ import Image from "next/image";
 
 const steps = ["Enter details", "Suggested Schemes"];
 interface Scheme {
-    beneficiaryState: string;
-    schemeShortTitle: string;
-    level: string;
-    nodalMinistryName: string;
-    schemeCategory: string;
-    schemeName: string;
-    schemeCloseDate: string;
-    briefDescription: string;
-    age_range: string;
-    tags: string[];
+  beneficiaryState: string;
+  schemeShortTitle: string;
+  level: string;
+  nodalMinistryName: string;
+  schemeCategory: string;
+  schemeName: string;
+  schemeCloseDate: string;
+  briefDescription: string;
+  age_range: string;
+  tags: string[];
 }
 
 export default function HorizontalLinearStepper() {
@@ -30,6 +29,7 @@ export default function HorizontalLinearStepper() {
   const { handleSubmit, watch } = useForm();
   const [schemeData, setSchemeData] = React.useState([]);
   const formValues = watch();
+
   const handleNext = () => {
     setIsLoading(true);
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
@@ -37,13 +37,16 @@ export default function HorizontalLinearStepper() {
       setIsLoading(false);
     }, 1000);
   };
-  const fetchSchemes = async() => {
+
+  const fetchSchemes = async () => {
     const response = await axios.get("http://127.0.0.1:8000/schemes");
     setSchemeData(response?.data);
-  }
-    React.useEffect(() => {
-        fetchSchemes();
-    }, []);
+  };
+
+  React.useEffect(() => {
+    fetchSchemes();
+  }, []);
+
   const handleReset = () => {
     setActiveStep(0);
     setIsLoading(false);
@@ -55,52 +58,66 @@ export default function HorizontalLinearStepper() {
     );
   };
 
-  const SkeletonCard = () => (
-    <Card className="h-36 w-full">
-      <CardHeader>
-        <Skeleton className="h-6 w-2/3" />
-      </CardHeader>
-      <CardContent>
-        <Skeleton className="h-4 w-full" />
-        <Skeleton className="mt-2 h-4 w-4/5" />
-      </CardContent>
+  const BardLogoOverlay = () => (
+    <Card className="flex h-screen w-full items-center justify-center">
+      <Image
+        src="/assets/Google_Bard_logo.svg"
+        width={48}
+        height={48}
+        alt="Bard logo"
+      />
+      <span className="ml-2 text-2xl">...</span>
+      <div className="absolute inset-0 bg-white opacity-50"></div>
     </Card>
   );
+
   const truncate = (input: string) => {
     if (input.length > 20) {
       return input.substring(0, 20) + "...";
     }
     return input;
-  }
-  const getPath = (index: number) => { 
-    return `/scheme/${index + 1}`; 
   };
-  const mappedCards = schemeData.sort(() => Math.random() - 0.5).slice(0, 4).map((scheme: Scheme, index: number) => (
-    <Link href={getPath(scheme?.schemeShortTitle)} key={index}>
-    <Card className="w-full p-4 border border-orange-400 rounded-xl border-orange">
-    <CardHeader className="p-0">
-      <div className="flex items-center space-x-2">
-        
-        <Image src="/assets/documents.png" className="pt-4" width={50} height={50} alt={scheme.schemeName} />
-        <h2 className="text-lg font-semibold text-left pl-1">{scheme.schemeName}</h2>
-        <ChevronRight className="h-6 w-6" />
-      </div>
-    </CardHeader>
-    <CardContent className="flex flex-end ml-8">
-      <div className="flex flex-wrap gap-2 mt-2">
-        {scheme.tags.map((tag: string, index: number) => (
-          <span
-            key={index}
-            className="px-3 py-1 text-sm border border-orange text-gray-800 rounded-full hover:bg-orange hover:text-white"
-          >
-            {truncate(tag)}
-          </span>
-        ))}
-      </div>
-    </CardContent>
-  </Card>
-  </Link>
-  ));
+
+  const getPath = (index: number) => {
+    return `/scheme/${index + 1}`;
+  };
+
+  const mappedCards = schemeData
+    .sort(() => Math.random() - 0.5)
+    .slice(0, 4)
+    .map((scheme: Scheme, index: number) => (
+      <Link href={getPath(scheme?.schemeShortTitle)} key={index}>
+        <Card className="border-orange w-full rounded-xl border border-orange-400 p-4">
+          <CardHeader className="p-0">
+            <div className="flex items-center space-x-2">
+              <Image
+                src="/assets/documents.png"
+                className="pt-4"
+                width={50}
+                height={50}
+                alt={scheme.schemeName}
+              />
+              <h2 className="pl-1 text-left text-lg font-semibold">
+                {scheme.schemeName}
+              </h2>
+              <ChevronRight className="h-6 w-6" />
+            </div>
+          </CardHeader>
+          <CardContent className="flex-end ml-8 flex">
+            <div className="mt-2 flex flex-wrap gap-2">
+              {scheme.tags.map((tag: string, index: number) => (
+                <span
+                  key={index}
+                  className="border-orange hover:bg-orange rounded-full border px-3 py-1 text-sm text-gray-800 hover:text-white"
+                >
+                  {truncate(tag)}
+                </span>
+              ))}
+            </div>
+          </CardContent>
+        </Card>
+      </Link>
+    ));
 
   return (
     <div className="w-full">
@@ -120,11 +137,7 @@ export default function HorizontalLinearStepper() {
           {activeStep === 1 && (
             <div className="mt-3 flex flex-col gap-3">
               <h2 className="mb-3 text-2xl">Suggested Schemes</h2>
-              {isLoading
-                ? Array.from({ length: 5 }).map((_, index) => (
-                    <SkeletonCard key={index} />
-                  ))
-                : mappedCards}
+              {isLoading ? <BardLogoOverlay /> : mappedCards}
             </div>
           )}
 
--- frontend/src/components/layout/header.tsx
+++ frontend/src/components/layout/header.tsx
@@ -2,6 +2,9 @@ import ThemeToggle from '@/components/layout/ThemeToggle/theme-toggle';
 import { cn } from '@/lib/utils';
 import { MobileSidebar } from './mobile-sidebar';
 import { UserNav } from './user-nav';
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import HorizontalLinearStepper from "@/components/Stepper";
 
 export default function Header() {
   return (
@@ -11,6 +14,18 @@ export default function Header() {
           <MobileSidebar />
         </div>
         <div className="flex items-center gap-2">
+        <div className="flex items-center space-x-2 md:ml-auto md:flex">
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button variant="button" className="h-10 w-28 p-6 text-lg">
+                  Add user
+                </Button>
+              </DialogTrigger>
+              <DialogContent className="h-full">
+                <HorizontalLinearStepper />
+              </DialogContent>
+            </Dialog>
+          </div>
           <UserNav />
           <ThemeToggle />
         </div>
